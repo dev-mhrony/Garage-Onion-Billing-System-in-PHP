c9cf28651402a4c0f10fd0f0c541e0726067c91e
MH RONY <78216965+dev-mhrony@users.noreply.github.com>
2023-04-16 23:45:47 +0600

    Update ajax_represent.php

--- ajax_represent.php
+++ ajax_represent.php
@@ -1,12 +1,3 @@
-<!-- /*!
-* Author Name: MH RONY.
-* GigHub Link: https://github.com/dev-mhrony
-* Facebook Link:https://www.facebook.com/dev.mhrony
-* Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
-* Visit My Website : developerrony.com
-*/ -->
-
 <?php
 //error_reporting(0);
 require_once('./constant/connect');
